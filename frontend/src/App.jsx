de5c1e5581a8408a2298d56002224d5ce3f8bb6d
Raj <117978228+LegiT30@users.noreply.github.com>
2025-01-21 22:40:20 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
 import Login from "./Components/Login"
-import Dashboard from "./Components/Dashboard"
 import Addcourse from "./Components/Addcourse"
 import Signup from "./Components/Signup"
 import StudentDashboard from "./Components/StudentDashboard"
@@ -16,7 +15,7 @@ function App() {
       <Routes>
         <Route path="/signup" element={<Signup/>}/>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        
         <Route path="/add-course" element={<Addcourse />} />
         <Route path="/student-dashboard" element={<StudentDashboard />} />
         <Route path="/instructor-dashboard" element={<CourseInstDasboard />} />
